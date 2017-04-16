4d7b69b5a12dc0d11ad36f5f6b86d4fc3331179e
ann-kiy <ann-kiy@mail.ru>
2017-04-16 19:26:20 +0700

    Add function TextBox

--- TDlist/MainWindow.xaml.cs
+++ TDlist/MainWindow.xaml.cs
@@ -24,5 +24,27 @@ namespace TDlist
         {
             InitializeComponent();
         }
+        TextBox[] textBox = new TextBox[5];
+        
+        void AddTextBox(int i, TextBox[] textBox, int h,int l){
+             
+            textBox[i] = new TextBox();
+            textBox[i].HorizontalAlignment = HorizontalAlignment.Left;
+            textBox[i].Height = 20;
+            textBox[i].Width = 346;
+            textBox[i].VerticalAlignment = VerticalAlignment.Top;
+            textBox[i].TextWrapping = TextWrapping.Wrap;
+            textBox[i].Margin = new Thickness(h, l,0,0);
+            MyGrid.Children.Add(textBox[0]);
+
+    }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            AddTextBox(0, textBox, 1, 1);
+               
+       }
+
+
     }
 }
